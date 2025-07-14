2cf3dc9ba6468473f1e0bc604c7308293498dafc
Damir <dimayu2882@gmail.com>
2025-07-14 19:32:43 +0300

    Initial commit

---
+++ src/common/assets.js
@@ -0,0 +1,13 @@
+export const allTextureKeys = {
+	hand: 'hand',
+};
+
+export const appTextures = {
+	[allTextureKeys.hand]: 'img/hand.webp',
+};
+export const audioAssets = {
+	build: 'audio/build.mp3',
+	buildTwo: 'audio/build2.mp3',
+	music: 'audio/music.mp3',
+	swoosh: 'audio/swoosh.mp3',
+};
